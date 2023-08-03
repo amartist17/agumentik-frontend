ae30929e9ecb8320705ff9a86b6d69dd974be9ac
krishanpareek809 <75416823+krishanpareek809@users.noreply.github.com>
2023-08-03 16:52:40 +0530

    template selection

---
+++ TemplateSelection/TemplateSelection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TemplateSelection.css";
+
+function TemplateSelection(){
+    return(
+        <div className="container_main">
+            <div className="container_main_image1">
+                <img src="https://marketplace.canva.com/EAFEOwL2BQo/2/0/1131w/canva-white-simple-invoice-S_aHY7Zi14A.jpg" />
+            </div>
+            <div className="container_main_image2">
+                <img src="https://cdn.vertex42.com/ExcelTemplates/Images/excel-invoice-template.png" />
+            </div>
+            <div className="container_main_image3">
+                <img src="https://marketplace.canva.com/EAFDOcWjOIA/1/0/1236w/canva-watercolor-neutral-professional-business-invoice-QZXVpJNbXHc.jpg" />
+            </div>
+        </div>
+    )
+}
+
+export default TemplateSelection;
